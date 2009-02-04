e123d62de3e5255aed19c9ce6ea44dde0330dd68
dmitry.a.konovalov <dmitry.a.konovalov@186c7d38-ed03-11dd-97d9-e7a3ff7b18fb>
2009-02-04 02:30:50 +0000

    webstart

---
+++ trunk/kingroup_v2_java/kingroup_v2/ant/webstart/v2/kingroup_v2_dev_keys.bat
@@ -0,0 +1,4 @@
+set PATH="C:\Program Files\Java\jdk1.6.0_10\bin\";%PATH%
+rem set PATH="C:\bin\j2sdk1.4.2\bin\";%PATH%
+keytool -genkey -keystore kingroup_v2_dev_keys -validity 3650 -alias kingroup_v2_dev
+pause
